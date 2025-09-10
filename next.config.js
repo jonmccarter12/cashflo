9e9e1837f345d7383aa99a7545d34e3d6bcf0766
jonmccarter12 <jonmccarter12@gmail.com>
2025-09-09 21:50:54 -0400

    Fix duplicate nextConfig in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
 
-module.exports = nextConfig/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-}
-
 module.exports = nextConfig
