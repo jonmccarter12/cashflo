a2dd3063aab9d734962303ba62da7fd9252d90b0
jonmccarter12 <jonmccarter12@gmail.com>
2025-09-09 21:42:11 -0400

    Remove old index.tsx

--- Cashflo/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const Dashboard = dynamic(() => import('../components/Dashboard'), {
-  ssr: false
-})
-
-export default function Home() {
-  return <Dashboard />
-}
